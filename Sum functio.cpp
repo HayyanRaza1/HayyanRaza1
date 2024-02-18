5219cd9637ebb0ce2915096e68843c8b2e67982f
HayyanRaza1 <160399073+HayyanRaza1@users.noreply.github.com>
2024-02-18 19:10:50 +0500

    Add files via upload

---
+++ Sum functio.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int sum(int  a, int b){
+	int sum = a + b;
+	return sum;
+}
+
+int main(){
+	
+	int a, b;
+	cin >> a >>b;
+	cout << sum(a , b);
+	
+	
+	
+}
+
+

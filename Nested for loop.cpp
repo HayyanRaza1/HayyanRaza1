56124857950bf88541cf01423ed149197a8898c4
HayyanRaza1 <160399073+HayyanRaza1@users.noreply.github.com>
2024-03-21 09:27:32 +0500

    Add files via upload

---
+++ Nested for loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+  for (int i = 1; i <=3; i++) {
+    	
+    	for(int j=1; j<=3; j++){
+    		cout<< i <<" "<<j<<endl;
+    		if(i >=15 && i >=15){
+    			cout<<"Printed 15 times";
+			}
+		}
+  }
+  
+  return 0;
+}
+

b57aea160b8333e376994e2dfbb2a2b2d11b0acc
HayyanRaza1 <160399073+HayyanRaza1@users.noreply.github.com>
2024-03-21 08:40:26 +0500

    Add files via upload

---
+++ Input name 15 times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+  for (int i = 0; i < 15; i++) {
+    cout << " Enter Your name"<< endl;
+	cin >> name;
+  }
+  
+  return 0;
+}
+
